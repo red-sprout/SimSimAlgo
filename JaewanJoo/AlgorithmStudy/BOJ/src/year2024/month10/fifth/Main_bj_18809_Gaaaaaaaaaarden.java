bd562c2ee4b146aaa59aa544dbf32757b4cc696b
Jaewan Joo <jjw05015@gmail.com>
2024-10-31 11:02:07 +0900

    [BOJ] 18809 / Gaaaaaaaaaarden / O

--- JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month10/fifth/Main_bj_18809_Gaaaaaaaaaarden.java
+++ JaewanJoo/AlgorithmStudy/BOJ/src/year2024/month10/fifth/Main_bj_18809_Gaaaaaaaaaarden.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class Main_bj_18809_Gaaaaaaaaaarden {
 	static int N, M, G, R, answer;
-	static int[][] map, greenDist, redDist;
+	static int[][] map, visited;
 	static List<int[]> list;
 	static int[] dr = {-1, 0, 1, 0};
 	static int[] dc = {0, -1, 0, 1};
@@ -31,61 +31,63 @@ public class Main_bj_18809_Gaaaaaaaaaarden {
 	static void initVisited() {
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
-				greenDist[i][j] = -1;
-				redDist[i][j] = -1;
+				visited[i][j] = 0;
 			}
 		}
 	}
 	static int countFlower(int[] green, int[] red) {
-		int cnt = 0;
-		for(int idx : green) {
-			int[] pos = list.get(idx);
-			greenQ.offer(pos);
-			greenDist[pos[0]][pos[1]] = 0;
-		}
-		for(int idx : red) {
-			int[] pos = list.get(idx);
-			redQ.offer(pos);
-			redDist[pos[0]][pos[1]] = 0;
-		}
+		init(green, greenQ, 1);
+		init(red, redQ, 2);
 		while(!greenQ.isEmpty() || !redQ.isEmpty()) {
-			int gSize = greenQ.size();
-			for(int i = 0; i < gSize; i++) {
-				int[] cur = greenQ.poll();
-				for(int d = 0; d < 4; d++) {
-					int nr = cur[0] + dr[d];
-					int nc = cur[1] + dc[d];
-					if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-					if(map[nr][nc] == 0 || greenDist[nr][nc] != -1) continue;
-					greenDist[nr][nc] = greenDist[cur[0]][cur[1]] + 1;
-					if(redDist[nr][nc] == -1) greenQ.offer(new int[] {nr, nc});
-				}
-			}
-			int rSize = redQ.size();
-			for(int i = 0; i < rSize; i++) {
-				int[] cur = redQ.poll();
-				for(int d = 0; d < 4; d++) {
-					int nr = cur[0] + dr[d];
-					int nc = cur[1] + dc[d];
-					if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
-					if(map[nr][nc] == 0 || redDist[nr][nc] != -1) continue;
-					redDist[nr][nc] = redDist[cur[0]][cur[1]] + 1;
-					if(greenDist[nr][nc] == -1) redQ.offer(new int[] {nr, nc});
-				}
-			}
+			updateLazy(greenQ);
+			updateLazy(redQ);
+			update();
 		}
-		System.out.println("G");
-		for(int[] a : greenDist) System.out.println(Arrays.toString(a));
-		System.out.println("R");
-		for(int[] a : redDist) System.out.println(Arrays.toString(a));
+		int cnt = 0;
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
-				if(greenDist[i][j] == -1 || redDist[i][j] == -1) continue;
-				if(greenDist[i][j] == redDist[i][j]) cnt++;
+				if(visited[i][j] == 3) cnt++;
 			}
 		}
-		System.out.println(cnt);
 		return cnt;
+	}
+	static void init(int[] start, Queue<int[]> q, int color) {
+		for(int idx : start) {
+			int[] pos = list.get(idx);
+			q.offer(new int[] {pos[0], pos[1], color});
+			visited[pos[0]][pos[1]] |= color;
+		}
+	}
+	static void updateLazy(Queue<int[]> q) {
+		int size = q.size();
+		for(int i = 0; i < size; i++) {
+			int[] cur = q.poll();
+			for(int d = 0; d < 4; d++) {
+				int nr = cur[0] + dr[d];
+				int nc = cur[1] + dc[d];
+				if(nr < 0 || nr >= N || nc < 0 || nc >= M) continue;
+				if(map[nr][nc] == 0 || visited[nr][nc] > 0) continue;
+				lazy.offer(new int[] {nr, nc, cur[2]});
+			}
+		}
+	}
+	static void update() {
+		int size = lazy.size();
+		for(int i = 0; i < size; i++) {
+			int[] cur = lazy.poll();
+			if((visited[cur[0]][cur[1]] & cur[2]) != 0) continue;
+			visited[cur[0]][cur[1]] |= cur[2];
+			lazy.offer(cur);
+		}
+		while(!lazy.isEmpty()) {
+			int[] cur = lazy.poll();
+			if(visited[cur[0]][cur[1]] == 3) continue;
+			if(cur[2] == 1) {
+				greenQ.offer(cur);
+			} else {
+				redQ.offer(cur);
+			}
+		}
 	}
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -96,8 +98,7 @@ public class Main_bj_18809_Gaaaaaaaaaarden {
 		G = Integer.parseInt(st.nextToken());
 		R = Integer.parseInt(st.nextToken());
 		map = new int[N][M];
-		greenDist = new int[N][M];
-		redDist = new int[N][M];
+		visited = new int[N][M];
 		list = new ArrayList<>();
 		for(int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
