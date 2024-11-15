3e212c562d0236a1f0ec0a26f2ebf9563a4b599e
Jaewan Joo <jjw05015@gmail.com>
2024-11-15 23:20:03 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    return 0;
+}
