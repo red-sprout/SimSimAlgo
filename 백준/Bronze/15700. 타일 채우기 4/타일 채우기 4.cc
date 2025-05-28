0c338731866e6d9e6900bf8669e016cf46f49054
Jaewan Joo <jjw05015@gmail.com>
2025-05-28 22:47:38 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    ll n, m; cin >> n >> m;
+    cout << n * m / 2 << '\n';
+    return 0;
+}
