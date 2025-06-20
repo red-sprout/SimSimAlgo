13bb19acbfd7bdbb43fb6985cb2b44f14a510b0d
Jaewan Joo <jjw05015@gmail.com>
2025-06-20 23:37:18 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int n; cin >> n;
+    for (int i = 0; i < n / 4; i++) {
+        cout << "long ";
+    }
+    cout << "int\n";
+    return 0;
+}
