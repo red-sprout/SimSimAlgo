5baf05fadaac803e5b0bc349fcbebee8a61ac222
Jaewan Joo <jjw05015@gmail.com>
2025-07-05 09:38:44 +0900

    [Gold III] Title: 종이접기, Time: 4 ms, Memory: 2276 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/20187. 종이접기/종이접기.cc
+++ 백준/Gold/20187. 종이접기/종이접기.cc
@@ -1,127 +1,76 @@
 #include <iostream>
-#include <cmath>
-#include <unordered_map>
 #include <vector>
-#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
 using namespace std;
 
-int k, n;
-vector<char> paper[1 << 8][1 << 8];
+int k, n, h;
+vector<char> fold;
+int paper[1 << 8][1 << 8];
 
-void fill_r(int cur, int target, int idx, char c) {
-    if (cur == target) {
-        if (c == '.') return;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < 1 << target; ++j) {
-                paper[i][idx + j].emplace_back(c);
-            }
-        }
+const int dr[] = {2, 3, 0, 1};
+const int dc[] = {1, 0, 3, 2};
+
+void recur(int fr, int fc, int sr, int sc, int er, int ec) {
+    if(fr == k && fc == k) {
+        paper[sr][sc] = h;
         return;
     }
-    int range = 1 << (cur - 1);
-    fill_r(cur - 1, target, idx, 'R');
-    fill_r(cur - 1, target, idx + range, '.');
-}
-
-void fill_l(int cur, int target, int idx, char c) {
-    if (cur == target) {
-        if (c == '.') return;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < 1 << target; ++j) {
-                paper[i][idx + j].emplace_back(c);
+    char f = fold[fr + fc];
+    switch(f) {
+        case 'D':
+            sr = (sr + er) >> 1;
+            recur(fr + 1, fc, sr, sc, er, ec);
+            for(int i = 0; i < 1 << (k - fr - 1); ++i) {
+                for(int j = sc; j < ec; ++j) {
+                    paper[sr - i - 1][j] = dr[paper[sr + i][j]];
+                }
             }
-        }
-        return;
-    }
-    int range = 1 << (cur - 1);
-    fill_l(cur - 1, target, idx, '.');
-    fill_l(cur - 1, target, idx + range, 'L');
-}
-
-void fill_d(int cur, int target, int idx, char c) {
-    if (cur == target) {
-        if (c == '.') return;
-        for (int i = 0; i < 1 << target; ++i) {
-            for (int j = 0; j < n; ++j) {
-                paper[idx + i][j].emplace_back(c);
+            break;
+        case 'U':
+            er = (sr + er) >> 1;
+            recur(fr + 1, fc, sr, sc, er, ec);
+            for(int i = 0; i < 1 << (k - fr - 1); ++i) {
+                for(int j = sc; j < ec; ++j) {
+                    paper[er + i][j] = dr[paper[er - i - 1][j]];
+                }
             }
-        }
-        return;
-    }
-    int range = 1 << (cur - 1);
-    fill_d(cur - 1, target, idx, 'D');
-    fill_d(cur - 1, target, idx + range, '.');
-}
-
-void fill_u(int cur, int target, int idx, char c) {
-    if (cur == target) {
-        if (c == '.') return;
-        for (int i = 0; i < 1 << target; ++i) {
-            for (int j = 0; j < n; ++j) {
-                paper[idx + i][j].emplace_back(c);
+            break;
+        case 'R':
+            sc = (sc + ec) >> 1;
+            recur(fr, fc + 1, sr, sc, er, ec);
+            for(int i = sr; i < er; ++i) {
+                for(int j = 0; j < 1 << (k - fc - 1); ++j) {
+                    paper[i][sc - j - 1] = dc[paper[i][sc + j]];
+                }
             }
-        }
-        return;
+            break;
+        default:
+            ec = (sc + ec) >> 1;
+            recur(fr, fc + 1, sr, sc, er, ec);
+            for(int i = sr; i < er; ++i) {
+                for(int j = 0; j < 1 << (k - fc - 1); ++j) {
+                    paper[i][ec + j] = dc[paper[i][ec - j - 1]];
+                }
+            }
+            break;
     }
-    int range = 1 << (cur - 1);
-    fill_u(cur - 1, target, idx, '.');
-    fill_u(cur - 1, target, idx + range, 'U');
-}
-
-int get_res(int r, int c, int hole) {
-    int arr[] = {0, 1, 2, 3};
-    for (auto ele : paper[r][c]) {
-        switch (ele) {
-            case 'R':
-            case 'L':
-                swap(arr[0], arr[1]);
-                swap(arr[2], arr[3]);
-                break;
-            case 'D':
-            case 'U':
-                swap(arr[0], arr[2]);
-                swap(arr[1], arr[3]);
-                break;
-        }
-    }
-    return arr[hole];
 }
 
 int main() {
-    FASTIO;
-    cin >> k; n = 1 << k;
-
-    char c;
-    unordered_map<char, int> cnt;
-    for (int i = 0; i < n; ++i) {
-        cin >> c;
-        cnt[c] += 1;
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> k;
+    n = 1 << k;
+    fold.resize(2 * k);
+    for(int i = 0; i < 2 * k; ++i) {
+        cin >> fold[i];
     }
-
-    for (auto e : cnt) {
-        switch (e.first) {
-            case 'R':
-                fill_r(k, k - e.second, 0, 'R');
-                break;
-            case 'L':
-                fill_l(k, k - e.second, 0, 'L');
-                break;
-            case 'D':
-                fill_d(k, k - e.second, 0, 'D');
-                break;
-            case 'U':
-                fill_u(k, k - e.second, 0, 'U');
-                break;
-        }
-    }
-
-    int hole; cin >> hole;
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cout << get_res(i, j, hole) << ' ';
+    cin >> h;
+    recur(0, 0, 0, 0, n, n);
+    for(int i = 0; i < n; ++i) {
+        for(int j = 0; j < n; ++j) {
+            cout << paper[i][j] << ' ';
         }
         cout << '\n';
     }
-
     return 0;
 }
