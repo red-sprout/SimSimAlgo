1d47572fed4a1b0e47819a62556acbbb7840f0e3
Jaewan Joo <jjw05015@gmail.com>
2025-07-17 10:11:43 +0900

    [Gold III] Title: 진법 변환, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1112. 진법 변환/진법 변환.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main() {
+    FASTIO;
+    int x, b; cin >> x >> b;
+    if (x == 0) {
+        cout << 0 << '\n';
+        return 0;
+    }
+
+    deque<int> a;
+    if (x > 0 && b > 0) {
+        while (x != 0) {
+            int mod = x % b;
+            x = x / b;
+            a.emplace_back(mod);
+        }
+        while (!a.empty()) {
+            cout << a.back();
+            a.pop_back();
+        }
+        cout << '\n';
+    } else if (x > 0 && b < 0) {
+        while (x != 0) {
+            int mod = x % b;
+            if (mod < 0) mod -= b;
+            x = (x - mod) / b;
+            a.emplace_back(mod);
+        }
+        while (!a.empty()) {
+            cout << a.back();
+            a.pop_back();
+        }
+        cout << '\n';
+    } else if (x < 0 && b > 0) {
+        x = -x;
+        while (x != 0) {
+            int mod = x % b;
+            x = x / b;
+            a.emplace_back(mod);
+        }
+        cout << '-';
+        while (!a.empty()) {
+            cout << a.back();
+            a.pop_back();
+        }
+        cout << '\n';
+    } else {
+        while (x != 0) {
+            int mod = x % b;
+            if (mod < 0) mod -= b;
+            x = (x - mod) / b;
+            a.emplace_back(mod);
+        }
+        while (!a.empty()) {
+            cout << a.back();
+            a.pop_back();
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
